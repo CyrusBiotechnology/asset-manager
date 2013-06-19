a43e4d19e5a08577728a03c2ccabc348af0456cd
noahg <noah.gunther@gmail.com>
2013-06-19 12:39:53 -0500

    titles

--- assets/assets/views.py
+++ assets/assets/views.py
@@ -171,8 +171,8 @@ def import_index(request):
 
 
 def create_object(request, model):
+	check_form(model)
 	title = 'Create'
-    check_form(model)
     object_template = 'generic/model-form.html'
 
     if model == 'checkout':
