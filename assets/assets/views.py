306ef090648adcc4e77427310aca5dd76ff860e9
noahg <noah.gunther@gmail.com>
2013-06-19 13:31:34 -0500

    titles

--- assets/assets/views.py
+++ assets/assets/views.py
@@ -171,7 +171,7 @@ def import_index(request):
 
 
 def create_object(request, model):
-    title = 'Create'
+    title = 'Create', model
     check_form(model)
 
     object_template = 'generic/model-form.html'
