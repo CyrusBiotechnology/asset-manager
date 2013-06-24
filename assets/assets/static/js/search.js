7f8cd289e481e01abfcb8eb3028062c57589875b
noahg <noah.gunther@gmail.com>
2013-06-24 12:44:47 -0500

    Signed-off-by: noahg <noah.gunther@gmail.com>

--- assets/assets/static/js/search.js
+++ assets/assets/static/js/search.js
@@ -43,7 +43,7 @@ $(function(){
   });
 
   $('.remove-filter').click(function(){
-    $('.search-filter').last().after($('.search-filter-template').last()remove();
+    $('.search-filter').last().after($('.search-filter-template').last()remove());
   };
   
   $(search_element).focus();
@@ -55,4 +55,4 @@ $(function(){
   $('.field-select').change(function(){
     query();
   });
-});
+}));
