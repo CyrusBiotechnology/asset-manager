9ad97d0d482a23850b9ce76e72bfa7c2780781f4
noahg <noah.gunther@gmail.com>
2013-06-24 13:35:06 -0500

    Signed-off-by: noahg <noah.gunther@gmail.com>

--- assets/assets/static/js/search.js
+++ assets/assets/static/js/search.js
@@ -2,7 +2,6 @@
 var search_element = '.filter';
 var results_element = '#search-results';
 var search_url = 'se?';
-var filter_count = 1;
 
 SR_template = '';
 
@@ -41,12 +40,10 @@ $(function(){
   $('.add-filter').click(function(){
     $('.search-filter').last().after($('.search-filter-template').last().clone(withDataAndEvents=true));
     $('.search-filter-template').last().removeClass('search-filter-template');
-	$(filter_count ++);
   });
 
   $('.remove-filter').click(function(){
     $('.search-filter').last().remove();
-	$(filter_count --);
   });
   
   $(search_element).focus();
