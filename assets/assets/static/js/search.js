9cdf2011d16dd4c588a942a15bec43e59a72a71f
noahg <noah.gunther@gmail.com>
2013-06-24 12:56:18 -0500

    adding (-) remove function

--- assets/assets/static/js/search.js
+++ assets/assets/static/js/search.js
@@ -43,7 +43,7 @@ $(function(){
   });
 
   $('.remove-filter').click(function(){
-    $('.search-filter').remove();
+    $('.search-filter').next().after($('.search-filter-template').next().remove();
   });
   
   $(search_element).focus();
