b07db8d4bd82821336e80fb16219ccec8f38c23d
noahg <noah.gunther@gmail.com>
2013-06-28 12:49:26 -0500

    adding error catching to csv_import

--- assets/assets/csv_import.py
+++ assets/assets/csv_import.py
@@ -65,7 +65,7 @@ def csv_import(request, uploaded_file_name, model_name):
     except IOError:
       print 'Please specify a file to upload'
 	  
-    except Tag.DoesNotExist:
+    except DoesNotExist:
       print 'Some specified fields do not exist in the database'
 		
     return {
