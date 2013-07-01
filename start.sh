301bf70734b455d09bb208e155a805a5530b608f
noahg <noah.gunther@gmail.com>
2013-07-01 09:06:39 -0500

    start.sh now activates the environment
    Signed-off-by: noahg <noah.gunther@gmail.com>

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo 'If this is your first time running the server setup.sh MUST be run first!'
 
+echo 'Activating the environment ...'
+source env/bin/activate
+
 echo 'Syncing the database ...'
 ./assets/manage.py syncdb --migrate
 
