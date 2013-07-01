242f4d2ffe75124d194292073bb407dc86b0c7d3
noahg <noah.gunther@gmail.com>
2013-07-01 10:36:18 -0500

    IOError catching for csv_import

--- assets/assets/csv_import.py
+++ assets/assets/csv_import.py
@@ -23,6 +23,7 @@ def csv_import(request, uploaded_file_name, model_name):
     with open(uploaded_file_name, 'rb') as csvfile:
         if upload_file_name == None:
           file_not_found == 1
+		  print 'No file specified'
         reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         first = True
         for row in reader:
