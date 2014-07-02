88613aee3b177765d51ac88b74c20e83e0578d41
Peter Novotnak <pnovotnak@flexion.us>
2014-07-02 13:09:11 -0500

    added compress to loads in base template

--- assets/assets/settings.py
+++ assets/assets/settings.py
@@ -77,8 +77,9 @@ INSTALLED_APPS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+
+    'compressor.finders.CompressorFinder',
 )
 
 # List of callables that know how to import templates from various sources.
